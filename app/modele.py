a388e190de948a41dc1d0d626bf30599473692c0
Wiktor <twardzikwiktor3@gmail.com>
2025-10-17 17:03:25 +0200

    Dokończenie metod w klasie Zadanie: na_slownik, ze_slownika

--- app/modele.py
+++ app/modele.py
@@ -5,7 +5,7 @@ from enum import Enum
 class StatusZadania(Enum):
     Oczekujace = "Oczekujące"
     W_Trakcie = "w_trakcie"
-    Zakończone = "Zakończone"
+    Zakonczone = "Zakończone"
 
 class Zadanie:
     def __init__(self,
@@ -20,4 +20,40 @@ class Zadanie:
         self.opis = opis
         self.status = status
         self.termin = termin
-        self.data_utowrzenia = data_utworzenia or datetime.now()
+        self.data_utworzenia = data_utworzenia or datetime.now()
+
+    def na_slownik(self) -> dict:
+        slownik = {
+            'id_zadania' : self.id_zadania,
+            'tytul' : self. tytul,
+            'opis' : self.opis,
+            'status' : self.status.value,
+            'data_utowrzenia' : self.data_utworzenia.isoformat()
+        }
+        if self.termin:
+            slownik['termin'] = self.termin.isoformat()
+        else:
+            slownik['termin'] = None
+
+
+    @classmethod
+    def ze_slownika(cls, dane: dict) -> 'Zadanie':
+        termin = None
+        if dane.get('termin'):
+            termin = datetime.fromisoformat(dane['termin'])
+
+        return cls(
+            id_zadania= dane.get('id_zadania'),
+            tytul= dane['tytul'],
+            opis= dane.get('opis', ''),
+            status= StatusZadania(dane['status']),
+            termin= termin,
+            data_utworzenia= datetime.fromisoformat(dane['data_utworzenia'])
+        )
+
+    def __str__(self) -> str:
+
+        pass
+    def czy_przeterminowane(self) -> bool:
+
+        pass
